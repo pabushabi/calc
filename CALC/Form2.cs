8104b5df9640c75d56e28e0ac714fc62f8371a1c
pabushabi <pabushabi@mail.ru>
2018-04-17 14:56:00 +0900

    Added history and fixed some bugs

---
+++ CALC/Form2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CALC;
+
+namespace CALC1
+{
+    public partial class Form2 : Form
+    {
+        public string history = "";
+        public string path = @"..\..\his.txt";
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    textBox1.Text += line + " ";
+                    textBox1.Text += '\r';
+                }
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.ResetText();
+            using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    textBox1.Text += line + " ";
+                    textBox1.Text += '\r';
+                }
+            }
+        }
+    }
+}
---
+++ CALC/calculator.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CALCULATOR
+{
+
+
+    class calculator
+    {
+
+        public double sub(ref double x, ref double y)
+        {
+            double res;
+            res = x - y;
+            return res;
+        }
+
+        public double add(ref double x, ref double y)
+        {
+            double res;
+            res = x + y;
+            return res;
+        }
+
+        public double div(ref double x, ref double y)
+        {
+            double res;
+            res = x / y;
+            return res;
+        }
+
+        public double mul(ref double x, ref double y)
+        {
+            double res;
+            res = x * y;
+            return res;
+        }
+
+        public double sinx(ref double x)
+        {
+            double res;
+            res = Math.Sin(x);
+            return res;
+        }
+
+        public double cosx(ref double x)
+        {
+            double res;
+            res = Math.Cos(x);
+            return res;
+        }
+
+        public double tanx(ref double x)
+        {
+            double res;
+            res = Math.Tan(x);
+            return res;
+        }
+
+        public int fac(ref int x)
+        {
+            int res = 1;
+            for (int i = 1; i <= x; i++)
+            {
+                res = res * i;
+            }
+            return res;
+        }
+
+        public double per(ref double x)
+        {
+            double res;
+            res = x / 100;
+            return res;
+        }
+
+        public double sqrtu(ref double x)
+        {
+            double res;
+            res = Math.Sqrt(x);
+            return res;
+        }
+
+        public double powu(ref double x, ref int y)
+        {
+            double res;
+            res = Math.Pow(x, y);
+            return res;
+        }
+
+        public double ln(ref double x)
+        {
+            double res;
+            res = Math.Log(x);
+            return res;
+        }
+
+    }
+}
